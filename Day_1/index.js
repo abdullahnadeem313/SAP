37ec0e61ddcf12800bb1257c8ea44374e877d45b
Abdullah Nadeem <mabdullahnadeem313@gmail.com>
2023-12-21 19:58:03 +0500

    Assignment 1,2,3

--- Day_1/index.js
+++ Day_1/index.js
@@ -6,9 +6,43 @@ console.log("Assignment 1");
 let brands = ["Dell", "HP", "ACER", "SONY", "PANASONIC", "OTHER"];
 
 let sortedBrands = brands
-  .filter(brand => brand !== "OTHER") // Exclude "OTHER" from the array
-  .sort() // Sort in ascending order
-  .concat("OTHER"); // Add "OTHER" at the end of the sorted array
+  .filter(brand => brand !== "OTHER") 
+  .sort() 
+  .concat("OTHER"); 
 
 console.log(sortedBrands);
 
+
+console.log("Assignment 2");
+// Assignment 2
+// 1. Create an array of String having values "Ali@test.COM", "Umair@test.com","Ahmad@test.com","Umair@Test.Com"
+// 2. Remove duplicate from the list.
+
+let emails = ["Ali@test.COM", "Umair@test.com", "Ahmad@test.com", "Umair@Test.Com"];
+
+let uniqueEmailsSet = new Set(emails);
+
+let uniqueEmails = Array.from(uniqueEmailsSet);
+
+console.log(uniqueEmails);
+
+console.log("Assignment 3");
+// Assignment 3
+// 1. let a = "[{Name:”umair”,Id:0 }{Nme:”ali”,Id:1}]"
+// 	change name of ID 1 from ali to Ali Chawla also
+// 	add attribute "Edit" with value true.
+
+let a = [
+  {
+    Name:"umair",
+    Id:0 
+  },
+  {
+    Name:"ali",
+    Id:1
+  }];
+  a[1].Name="Ali Chawla";
+  // a[1].Edit=true;
+  b= {edit:true}
+  a.push(b)
+  console.log(a);
