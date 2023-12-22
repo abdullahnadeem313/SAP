70126d9b31f015b584110e0bdf57adfaa1b25618
Abdullah Nadeem <mabdullahnadeem313@gmail.com>
2023-12-23 01:15:25 +0500

    Day 2 Assignment

--- Day_2/Assignment.js
+++ Day_2/Assignment.js
@@ -1,4 +1,4 @@
-console.log("Assignment 1");
+
 
 // Write a JavaScript function named fetchData that takes a callback function as an argument. 
 // Simulate an asynchronous operation (e.g., setTimeout) within fetchData 
@@ -12,11 +12,11 @@ function fetchData(callback) {
 
 //callback
 fetchData(() => {
+    console.log("Assignment 1");
     console.log("Data fetch Successful with callbacks");
 })
 
 
-console.log("Assignment 2");
 // Write a function createPromise that returns a Promise. 
 // The Promise should resolve after a random time delay (between 1 to 5 seconds) 
 // and should return a message like "Promise resolved after X seconds."
@@ -31,6 +31,7 @@ function createPromise() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve("Success");
+            console.log("Assignment 2");
             console.log(`Promise 1 resolved after ${randomNum/1000} seconds`);
         },randomNum );
     })
@@ -40,7 +41,6 @@ createPromise();
 
 
 
-console.log("Assignment 3");
 // Write a function rejectPromise that returns a Promise. 
 // The Promise should reject after a random time delay (between 1 to 5 seconds) 
 // with an error message like "Promise rejected after X seconds." 
@@ -56,17 +56,18 @@ function rejectPromise() {
 
 let rejectedPromise = rejectPromise()
 rejectedPromise.catch((err)=>{
-    console.log(err);
+        console.log("Assignment 3");
+        console.log("Error occured: " + err);
 })
 
 
-console.log("Assignment 4");
 // Convert the fetchData function from Question 1 to use async/await instead of callbacks. 
 // Ensure it still behaves asynchronously 
 // and logs the message after the simulated operation is complete.
 
 function getData () {
     setTimeout(() => {
+        console.log("Assignment 4");
         console.log("Data fetch successful with Aync Await");
     }, 2000);
 }
